b6e178dbd2e54c7d6cad0e8968d9ae36e7b075b1
Mustafa S Salman <cpu.salman@gmail.com>
2023-12-11 14:10:17 -0600

    Update sort_fnc.m

--- bin/autolabeller/sort_fnc.m
+++ bin/autolabeller/sort_fnc.m
@@ -1,11 +1,28 @@
 % This function modularizes an FNC matrix
 % Inputs: 
 %     fnc:     unsorted FNC matrix
-%     labels:  corresponding IC#, RSN/noise label and functional domain name. Example label input can be as shown:
-% 
+%     labels:  A 3-column cell. The columns are:
+%         IC#:                     should correspond to the row/column number of the FNC matrix input
+%         network/noise label:     a column of 1/0 in case the FNC matrix contains both network and noise entries
+%         functional domain label: a string containing the functional domain name of the IC. ICs belonging to each domain is modularized separately.
+%         
+%        Example labels input:
+%
 %         35,1,Visual
 %         16,1,Default
 %         36,1,Subcortical
+%         ...
+% 
+% Prerequisite:
+%     The reorder_mod function comes from the BCT toolbox- https://github.com/brainlife/BCT/tree/main/BCT/2019_03_03_BCT. It needs to be in Matlab path.
+% 
+% Note:
+%     If the whole matrix needs to be modularized and no label is needed, then a dummy labels cell like this should work-
+%         
+%         1,1,""
+%         2,1,""
+%         3,1,""
+%         ...
 % 
 function [network_idx_reordered, reordered_matrix, order_] = sort_fnc( fnc, labels )
     % sorted domain labels
