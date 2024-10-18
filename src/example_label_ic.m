e58134aa12a1e204b5a4528fe842af4bf4bd0176
katkaz18 <57729174+katkaz18@users.noreply.github.com>
2024-10-18 11:14:41 +0200

    Update example_label_ic.m

--- src/example_label_ic.m
+++ src/example_label_ic.m
@@ -2,15 +2,15 @@ clear all
 tStart = tic;
 
 % add requirements to path
-addpath( genpath( '../bin/GroupICATv4.0b/' ) )      % GIFT toolbox
-addpath( '/trdapps/linux-x86_64/matlab/toolboxes/spm12/' )      % SPM12 toolbox
-addpath( '../bin/2019_03_03_BCT' )       % Brain connectivity toolbox
-addpath( '../bin/autolabeller/' )       % add the autolabeller src folder only
+addpath( '../GroupICATv4.0b/' )      % GIFT toolbox
+addpath( '/matlab/toolboxes/spm12/' )      % SPM12 toolbox
+addpath( '../2019_03_03_BCT' )       % Brain connectivity toolbox
+addpath( '../autolabeller/' )       % add the autolabeller src folder only
 
 % GICA example with fbirn dataset
 clear params;
-params.param_file = '/data/users2/salman/projects/fBIRN/current/data/ICAresults_C100_fbirn/fbirnp3_rest_ica_parameter_info.mat';
-params.outpath = '../results/fbirn_debug/';
+params.param_file = 'ica_parameter_info.mat';
+params.outpath = './results';
 params.fit_method = 'mnr';
 params.n_corr = 3;
 params.skip_noise = 0;
@@ -22,14 +22,14 @@ params.threshold = 3;
 params.functional_atlas = 'yeo_buckner';
 % params.functional_atlas = 'gordon2016';
 % params.functional_atlas = 'caren';
-disp( 'Running the autolabeller on FBIRN dataset' )
+disp( 'Running the autolabeller on the selected dataset' )
 label_auto_main( params );
 
 % Spatial map example with neuromark template
 clear params;
-params.sm_path = '/data/mialab/competition2019/NetworkTemplate/NetworkTemplate_High_VarNor.nii';
+params.sm_path = '/Spatial_Maps.nii';
 params.mask_path = '/data/mialab/competition2019/NetworkTemplate/Mask.img';
-params.outpath = '../results/neuromark_debug/';
+params.outpath = './results/';
 params.fit_method = 'mnr';
 params.n_corr = 3;
 params.skip_noise = 0;
@@ -41,7 +41,7 @@ params.threshold = 3;
 params.functional_atlas = 'yeo_buckner';
 % params.functional_atlas = 'gordon2016';
 % params.functional_atlas = 'caren';
-disp( 'Running the autolabeller on NeuroMark dataset' )
+disp( 'Running the autolabeller on the selected dataset' )
 label_auto_main( params );
 
 tEnd = toc(tStart)
